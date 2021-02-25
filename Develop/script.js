2e04f33567cae8b1e39191ba4220e9c88da9fda2
etown285 <78112738+etown285@users.noreply.github.com>
2021-02-25 00:29:25 -0500

    created strings to create random variables and started for password generation and add special characters section to prompt

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,9 @@ generateBtn.addEventListener("click", writePassword);
 // Define all the available characters
 
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var characterselection = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numericcaseChar = "0123456789";
 
 // create a variable to hold all available possibilities
 var choiceArray = [];
@@ -26,24 +28,60 @@ function generatePassword() {
   return ''
 }
 
+function getRandomLowerChar() {
+  /*
+  for (var i = 0; i < lowercaseChar.length; i++) {
+    alert(lowercaseChar.charAt(i));
+  } */
+  return lowercaseChar.charAt(getRandomInt(lowercaseChar.length));
+}
+
+function getRandomUpperChar() {
+
+  return uppercaseChar.charAt(getRandomInt(uppercaseChar.length));
+}
+
+function getRandomCharacterSelection() {
+
+  return characterselection.charAt(getRandomInt(characterselection.length));
+}
+
+
+function getRandomnumericcaseChar() {
+
+  return numericcaseChar.charAt(getRandomInt(numericcaseChar.length));
+}
+
 
 function promptUser() {
   // Prompt User to choose if they want to use UPPER case letters
   var passLength = prompt("How long would you like your password?");
-  // Validate the lenght
+  // Validate the length
   if(passLength >= 8 && passLength < 128) {
     // if both expressions evaluate to true
     console.log("Valid Length");
-    
-    var lowercaseChar = confirm("Do you need lower case characters");
-    //Select lower case characters 
+    }
+  else {
+    return alert("Password Length must be between 8 and 128 characters");
+    }
+    var lowercaseChar = confirm("Do you need lower case characters?");
+    //Select lowercase characters 
     if(lowercaseChar === true) {
       
     }
-  } else {
-    return alert("Password Length must be between 8 and 128 characters");
-  }
+    var uppercaseChar = confirm("Do you need upper case characters?");
+    //Select uppercase characters 
+    if(uppercaseChar === true) {
+      
+    }
+    var numericcaseChar = confirm("Do you need numeric case characters?");
+    //Select numeric characters
+    if(numericcaseChar === true) {
 
-
-  console.log(passLength);
+    } 
+    var characterselection = confirm("Do you need special characters?");
+    //Select numeric characters
+    if(characterselection === true) {
+      
+    } 
 }
